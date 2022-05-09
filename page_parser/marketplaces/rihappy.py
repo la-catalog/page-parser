7f8d65d42419f4980c65cec92dc772549ba2dd82
thiagola92 <thiagola92@gmail.com>
2022-05-09 08:49:48 -0300

    Update to get informations from json

--- page_parser/marketplaces/rihappy.py
+++ page_parser/marketplaces/rihappy.py
@@ -4,8 +4,9 @@ from collections.abc import Generator
 
 from babel.numbers import parse_decimal
 from la_deep_get import dget
-from page_sku import SKU, Attribute
+from page_sku import SKU, Attribute, Price
 from parsel import Selector, SelectorList
+from url_parser import Parser as UrlParser
 
 from page_parser.abstractions import Marketplace
 
@@ -17,55 +18,61 @@ class Rihappy(Marketplace):
 
     def __init__(self, logger) -> None:
         self._logger = logger
+        self._url_parser = UrlParser(logger)
 
     def parse(self, text: str, url: str) -> Generator[list[SKU], tuple[str, str], None]:
         selector = Selector(text=text)
 
-        product = selector.xpath(
+        json_ = selector.xpath(
             "//template[@data-varname='__STATE__']/script/text()"
-        ).get()
-        product = json.loads(product) if product else None
+        ).get("{}")
+        json_: dict = json.loads(json_)
+        product = dget(list(json_.keys()), 0)
+        product = json_.get(product, {})
 
-        name = selector.xpath(
-            "//h1[contains(@class, 'vtex-store-components-3-x-productNameContainer')]//text()"
-        ).get()
+        name = product.get("productName")
+        code = product.get("productId")
+        brand = product.get("brand")
+        description = product.get("description")
 
-        code = selector.xpath(
-            "//span[@class='vtex-product-identifier-0-x-product-identifier__value']/text()"
-        ).get()
-
-        price_1 = selector.xpath(
-            "(//span[contains(@class, 'vtex-product-price-1-x-currencyContainer')])[1]//text()"
-        ).getall()
-        price_1 = "".join(price_1)
-        price_1 = re.sub(r"[^0-9,]", "", price_1)
-        price_1 = parse_decimal(price_1, locale="pt_BR") if price_1 else None
-
-        price_2 = selector.xpath(
-            "(//span[contains(@class, 'vtex-product-price-1-x-currencyContainer')])[3]//text()"
-        ).getall()
-        price_2 = "".join(price_2)
-        price_2 = re.sub(r"[^0-9,]", "", price_2)
-        price_2 = parse_decimal(price_2, locale="pt_BR") if price_2 else None
+        segments: list[str] = dget(product, "categories", "json") or []
+        segments: str = dget(segments, 0, default="")
+        segments: list[str] = segments.split("/")
+        segments = [segment for segment in segments if segment]
 
         currency = selector.xpath(
             "//span[contains(@class, 'vtex-product-price-1-x-currencyCode')]/text()"
-        ).get()
+        ).get("R$")
 
-        print(description)
+        prices: str = dget(product, "priceRange", "id")
+        prices: dict = json_.get(prices, {})
+        prices: list[str] = [dget(value, "id") for value in prices.values()]
+        prices_high: list[float] = [dget(json_, id_, "highPrice") for id_ in prices]
+        prices_low: list[float] = [dget(json_, id_, "lowPrice") for id_ in prices]
+        prices: set[str] = set(prices_high + prices_low)
+        prices: set[str] = set(price for price in prices if price)
+        prices: list[Price] = [
+            Price(amount=price, currency=currency) for price in prices
+        ]
 
-        attributes: list[Selector] = selector.xpath(
-            "//div[contains(@class, 'vtex-flex-layout-0-x-flexRow--product-specification-value')]"
-        ).getall()
-
-        print(attributes)
-
-        attributes = [
+        attributes: list = dget(product, "specificationGroups", default=[])
+        attributes: str = dget(attributes, -1, "id")
+        attributes: list[dict] = dget(json_, attributes, "specifications", default=[])
+        attributes: list[str] = [attribute.get("id") for attribute in attributes]
+        attributes: list[dict] = [json_.get(attribute) for attribute in attributes]
+        attributes: list[Attribute] = [
             Attribute(
-                name=attribute.xpath(".//div/div[1]/text()"),
-                value=attribute.xpath(".//div/div[2]/text()"),
+                name=attribute.get("name"),
+                value=". ".join(dget(attribute, "values", "json", default=[])),
             )
             for attribute in attributes
         ]
 
+        print(name)
+        print(code)
+        print(brand)
+        print(description)
+        print(segments)
+        print(currency)
+        print(prices)
         print(attributes)
